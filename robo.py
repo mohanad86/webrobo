65bf5ceac062544ce4656d8734746782ad6ba63e
mohanad86 <mohanad86@github.com>
2016-05-25 19:07:41 +0300

    Removing the unused code, editing the code style

--- robo.py
+++ robo.py
@@ -1,18 +1,13 @@
 import os
 from time import sleep
 from threading import Thread
-from flask import Flask, render_template, Response
+from flask import Flask
+from flask import render_template
+from flask import Response
 import cv2
 import json
 import NetworkManager
-for pin  in range(192,196):
-    try:
-        with open("/sys/class/gpio/export", "w") as fh:
-            fh.write(str(pin))
-    except IOError:
-        pass
-    with open("/sys/class/gpio/gpio%d/direction" % pin, "w") as fh:
-        fh.write("in")
+
 
 class MotorThread(Thread):
     def __init__(self, pin=192):
@@ -38,10 +33,12 @@ class MotorThread(Thread):
             else:
                 sleep(0.020)
 
+
 left = MotorThread(202)
 left.start()
-r2 = MotorThread(196)
-r2.start()
+right = MotorThread(196)
+right.start()
+
 app = Flask(__name__ )
 
 @app.route('/camera')
@@ -56,8 +53,10 @@ def index():
             yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + jpeg.tostring() + b'\r\n\r\n' 
     return Response(camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route("/api/wireless")
+
 def wireless():
     networks = []
+	#Getting the information about the network
     for dev in NetworkManager.NetworkManager.GetDevices():
         if dev.DeviceType != NetworkManager.NM_DEVICE_TYPE_WIFI:
             continue
@@ -68,10 +67,11 @@ def wireless():
 @app.route("/batterystatus")
 def battery():
     stats = {}
+	#open the file battery to get the information about the battery
     for filename in os.listdir("/sys/power/axp_pmu/battery/"):
         with open ("/sys/power/axp_pmu/battery/" + filename) as fh:
             stats[filename] = int(fh.read())
-            #r means read
+            #r means read the values 
     with open("/sys/class/gpio/gpio192/value", "r") as fh:
         stats["enemy_left"] = int(fh.read())
     with open("/sys/class/gpio/gpio193/value", "r") as fh:
@@ -85,9 +85,11 @@ def battery():
 @app.route("/css.css")
 def css():
     return app.send_static_file('css.css')
+    
 @app.route("/app.js")
 def java():
     return app.send_static_file('app.js')
+
 @app.route("/")
 def robot():
     return app.send_static_file('robot.html')
@@ -95,30 +97,34 @@ def robot():
 @app.route("/left")
 def command():
     left.speed = 1
-    r2.speed = -1
-    return "ok"
+    right.speed = -1
+    return "left"
+
 @app.route("/stop")
 def stop():
     left.speed = 0
-    r2.speed = 0
-    return "ok"
+    right.speed = 0
+    return "stop"
 
 @app.route("/go")
 def go():
     left.speed = 1
-    r2.speed = 1
-    return "ok"
+    right.speed = 1
+    return "go"
 
 @app.route("/right")
-def right():
+def right1():
     left.speed = -1
-    r2.speed = 1
-    return "ok"
+    right.speed = 1
+    return "right"
+
 @app.route("/back")
 def back():
     left.speed = -1
-    r2.speed = -1
-    return "ok"
+    right.speed = -1
+    return "back"
+    
+#if any error happened use (debug = True)
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", debug = True, threaded = True)
+    app.run(host = "0.0.0.0", threaded = True)
